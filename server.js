aec6d6ed598e150f13b3abbc42dd84c111ed7434
geet <34719351+Geet0560@users.noreply.github.com>
2023-06-28 23:43:21 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const ShortUrl = require('./models/shortUrl');
-const methodOverride = require('method-override'); 
+const methodOverride = require('method-override');
+const Favorite = require('./models/favorite');
 const app = express();
 
 mongoose.connect('mongodb://127.0.0.1/urlShortener', {
@@ -12,42 +13,56 @@ mongoose.connect('mongodb://127.0.0.1/urlShortener', {
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: false }));
 
-// process of adding the method
-app.use(methodOverride('_method')); 
+app.use(methodOverride('_method'));
 
 app.get('/', async (req, res) => {
-    let query = req.query.q; 
-    let searchCriteria = {}; 
-  
-    if (query) {
-        const regex = new RegExp(query, 'i');
-        searchCriteria = {
-          $or: [
-            // searching through all the three
-            { full: regex },
-            { short: regex }, 
-            { notes: regex}
-          ],
-        };
+  const filter = req.query.filter;
+
+  let shortUrls;
+  if (filter === 'alphabetical') {
+    shortUrls = await ShortUrl.find({}).sort({ notes: 1 });
+  } else if (filter === 'time') {
+    shortUrls = await ShortUrl.find({}).sort({ createdAt: -1 });
+  } else if (filter === 'length') {
+    shortUrls = await ShortUrl.aggregate([
+      {
+        $project: {
+          full: 1,
+          short: 1,
+          clicks: 1,
+          notes: 1,
+          length: { $strLenCP: '$notes' }
+        }
+      },
+      { $sort: { length: 1 } },
+      {
+        $project: {
+          full: 1,
+          short: 1,
+          clicks: 1,
+          notes: 1
+        }
       }
-  
-    try {
-      // Finding the matching URLs based on search criteria
-      const shortUrls = await ShortUrl.find(searchCriteria); 
-      // Passing the search results and query to the viewing
-      res.render('index', { shortUrls: shortUrls, query: query }); 
-    } catch (error) {
-      console.error('Error occurred while searching URLs:', error);
-      res.status(500).send('Internal Server Error');
-    }
-  });
+    ]);
+  } else {
+    shortUrls = await ShortUrl.find({});
+  }
+
+  try {
+    const favoriteUrls = await ShortUrl.find({ favorite: true });
+    res.render('index', { shortUrls: shortUrls, favoriteUrls: favoriteUrls });
+  } catch (error) {
+    console.error('Error occurred while retrieving URLs:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
 
 app.post('/shortUrls', async (req, res) => {
-    const { fullUrl, notes } = req.body; 
-    await ShortUrl.create({ full: fullUrl, notes: notes }); 
-    res.redirect('/');
-  });
-  
+  const { fullUrl, notes } = req.body;
+  await ShortUrl.create({ full: fullUrl, notes: notes });
+  res.redirect('/');
+});
+
 app.get('/:shortUrl', async (req, res) => {
   const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl });
   if (shortUrl == null) return res.sendStatus(404);
@@ -56,9 +71,40 @@ app.get('/:shortUrl', async (req, res) => {
   res.redirect(shortUrl.full);
 });
 
+app.put('/shortUrls/:id/favorite', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const shortUrl = await ShortUrl.findById(id);
+    if (!shortUrl) {
+      return res.status(404).send('URL not found');
+    }
 
+    // Toggle the favorite field
+    shortUrl.favorite = !shortUrl.favorite;
+    await shortUrl.save();
 
-// This will be handling the delete request for removing the entry
+    if (shortUrl.favorite) {
+      // Create a new favorite entry
+      const favorite = await Favorite.create({
+        full: shortUrl.full,
+        short: shortUrl.short,
+        clicks: shortUrl.clicks,
+        notes: shortUrl.notes
+      });
+      console.log('Favorite created:', favorite);
+    } else {
+      // Remove the corresponding favorite entry if unfavorited
+      await Favorite.deleteOne({ short: shortUrl.short });
+      console.log('Favorite deleted:', shortUrl.short);
+    }
+
+    res.redirect('/');
+  } catch (error) {
+    console.error('Error occurred while favoriting/unfavoriting URL:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
 app.delete('/shortUrls/:id', async (req, res) => {
   const { id } = req.params;
   await ShortUrl.findByIdAndRemove(id);
